73198421f9293ddb5d349244031e14a3c1e05158
Haoran Sun <hsun960120@gmail.com>
2017-11-20 22:05:41 -0800

    Postprocessing adeded.

--- src/MetaMap.java
+++ src/MetaMap.java
@@ -1,8 +1,17 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.jayway.jsonpath.JsonPath;
 
 import gov.nih.nlm.nls.skr.GenericObject;
 
@@ -12,11 +21,13 @@ import gov.nih.nlm.nls.skr.GenericObject;
  *
  */
 public class MetaMap {
+  private static String QUERY = "$.AllDocuments[0].Document.Utterances.*";
+  
   private GenericObject submission; //Web API submission
   private String email;
   
   public MetaMap(String username, String password, String email) {
-    this.submission = new GenericObject(Integer.MAX_VALUE, username, password);
+    this.submission = new GenericObject(100, username, password);
     this.email = email;
   }
   
@@ -61,11 +72,11 @@ public class MetaMap {
     this.submission.setField("Email_Address", email);
     this.submission.setField("APIText", sb.toString());
     this.submission.setField("KSOURCE", "1516");
-    String arguments = "--JSONf 2 ";
+    String arguments = "--JSONf 2 -A ";
     if(args != null)
       arguments += args;
     this.submission.setField("COMMAND_ARGS", arguments);
-    return this.submission.handleSubmission();
+    return this.postprocess(this.submission.handleSubmission());
   }
   
   private boolean syntaxCheck(String inclusion, String exclusion) {
@@ -89,14 +100,32 @@ public class MetaMap {
           return false;
     return true;
   }
+  
+  private String postprocess(String raw) {
+    String jsonStr = raw.substring(raw.indexOf('{'));
+    jsonStr = JsonPath.parse(jsonStr).read(QUERY).toString();
+    JSONArray jsonArr = null;
+    try {
+      jsonArr = new JSONArray(jsonStr);
+      for(int i = 0; i < jsonArr.length(); i++) {
+        JSONObject jsonObj = jsonArr.getJSONObject(i);
+        jsonObj.remove("PMID");
+        jsonObj.remove("UttSection");
+      }
+    } catch (JSONException e) {
+      e.printStackTrace();
+      return null;
+    }
+    return jsonArr.toString();
+  }
+  
   public static void main(String[] args) {
     MetaMap req = new MetaMap(args[0], args[1], args[2]);
     String opts = req.config("2017AA", "USAbase", null, null, null, null);
-    try
-    {
+    try(BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new 
+        FileOutputStream("results.json")))) {
        String results = req.getResults(args[3], opts);
-       System.out.print(results);
-
+       writer.write(results);
     } catch (RuntimeException ex) {
        System.err.println("");
        System.err.print("An ERROR has occurred while processing your");
@@ -107,7 +136,10 @@ public class MetaMap {
        System.err.println("");
        System.err.println("Trace:");
        ex.printStackTrace();
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
-
   }
 }
